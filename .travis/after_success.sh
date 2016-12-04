010c1e82f9d486a0e5dba0899269b7139000f926
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2016-12-04 19:36:24 +0200

    codecov.io integration

---
+++ .travis/after_success.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ "$CC" = "clang" ]; then
+	GCOVEXE=gcov
+	GCOVARGS=""
+else
+	GCOVEXE=llvm-cov
+	GCOVARGS="gcov"
+fi
+
+/bin/bash <(curl -s https://codecov.io/bash) -x "$GCOVEXE" -a "$GCOVARGS"
