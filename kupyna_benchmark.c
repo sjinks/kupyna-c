58ce10167c56d984bc15cd175861a892527b7a87
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2016-12-06 19:08:02 +0200

    K256: algorithm optimization

--- kupyna_benchmark.c
+++ kupyna_benchmark.c
@@ -7,7 +7,7 @@
 #define BUFFER_MIB   100
 #define BUFFER_SIZE  (1048576*BUFFER_MIB)
 
-static uint8_t buf[BUFFER_SIZE];
+_Alignas(16) static uint8_t buf[BUFFER_SIZE];
 
 int main(int argc, char** argv)
 {
@@ -25,6 +25,14 @@ int main(int argc, char** argv)
     t2 = clock();
     duration = (double)(t2 - t1) / CLOCKS_PER_SEC;
     printf("kupyna256: %.3f sec, %.3f MBps\n", duration, BUFFER_MIB/duration);
+
+    t1 = clock();
+    kupyna256_init(&k256);
+    kupyna256_update_aligned(&k256, buf, BUFFER_SIZE);
+    kupyna256_final(&k256, h);
+    t2 = clock();
+    duration = (double)(t2 - t1) / CLOCKS_PER_SEC;
+    printf("kupyna256a: %.3f sec, %.3f MBps\n", duration, BUFFER_MIB/duration);
 
     t1 = clock();
     kupyna512_init(&k512);
